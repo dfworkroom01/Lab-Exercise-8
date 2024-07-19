89cc922e363489cce8ad1108f74b34168a2da3dd
dfworkroom01 <dfworkroom01@gmail.com>
2024-07-19 15:54:39 +0800

    Rolling dice function

---
+++ rollDice.js
@@ -0,0 +1,18 @@
+//Function to roll the dice and produce a random result
+function rollDicea(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+ 
+}
+
+//Function to roll the dice and produce a random result
+function rollDiceb(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+ 
+  
+}
+
+//Function to roll the dice and produce a random result
+function rollDicec(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+ 
+}
